6df8faabeb949607c7ee3986076207d09b7b880f
Andrey <agafon404@gmail.com>
2021-08-24 18:17:27 +0300

    added bot functionality

--- homework.py
+++ homework.py
@@ -1,12 +1,19 @@
 import os
 import time
+import datetime
 import requests
 import telegram
+import logging
+import sys
 from dotenv import load_dotenv
 
-
+homeworks = {}
 load_dotenv()
-
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log', 
+    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s'
+)
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -14,37 +21,56 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 # проинициализируйте бота здесь,
 # чтобы он был доступен в каждом нижеобъявленном методе,
 # и не нужно было прокидывать его в каждый вызов
-bot = ...
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    status = homework['status']
+    if (homework_name not in homeworks.keys()):
+        homeworks[homework_name] = status
+    if (status == 'rejected'):
         verdict = 'К сожалению, в работе нашлись ошибки.'
-    else:
+    elif (status == 'approved'):
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
+    else:
+        return (logging.info('Работа пока не проверена'))
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+    homework_statuses = requests.get(url, headers=headers, params=payload)
     return homework_statuses.json()
 
 
 def send_message(message):
-    return bot.send_message(...)
+    logging.info('Отправлено сообщение')
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    current_timestamp = int(time.time())  # Начальное значение timestamp
-
+    logging.debug('Бот был запущен')
+    # случайная дата, с начала которой домашек не сдавалось
+    date = datetime.date(year=2021, month=8, day=23)
+    timestamp = time.mktime(date.timetuple())  # Начальное значение timestamp
     while True:
         try:
-            ...
+            homework = get_homeworks(int(timestamp))
+            if (homework['homeworks']):
+                if (homework['status'] != 'reviewing'):
+                    text = parse_homework_status(homework)
+                    send_message(text)
+                    sys.exit()
+            else:
+                logging.info('Домашек.нет')
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
-
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
+            text = f'Бот упал с ошибкой: {e}'
+            logging.error(text)
+            send_message(text)
             time.sleep(5)
 
 
