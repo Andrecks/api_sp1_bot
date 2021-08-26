3bc1a41276cd455065c07041132411409a854995
Andrey <agafon404@gmail.com>
2021-08-26 14:13:27 +0300

    happy little changes

--- homework.py
+++ homework.py
@@ -7,7 +7,9 @@ import logging
 import sys
 from dotenv import load_dotenv
 
-homeworks = {}
+verdicts = {'approved': 'Ревьюеру всё понравилось, работа зачтена!',
+            'rejected': 'К сожалению, в работе нашлись ошибки.',
+            }
 load_dotenv()
 logging.basicConfig(
     level=logging.DEBUG,
@@ -21,52 +23,55 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 # проинициализируйте бота здесь,
 # чтобы он был доступен в каждом нижеобъявленном методе,
 # и не нужно было прокидывать его в каждый вызов
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
-url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+Bot = telegram.Bot(token=TELEGRAM_TOKEN)
+Url = 'https://praktikum.yandex.ru/'
+HW_count = 5  # Переменная для подсчета успешно сданных домашек.
+# на данный момент проверенных там 5.
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    status = homework['status']
-    if (homework_name not in homeworks.keys()):
-        homeworks[homework_name] = status
-    if (status == 'rejected'):
-        verdict = 'К сожалению, в работе нашлись ошибки.'
-    elif (status == 'approved'):
-        verdict = 'Ревьюеру всё понравилось, работа зачтена!'
+    global HW_count
+    try:
+        homework_name = homework['homework_name']
+        status = homework['status']
+    except IndexError as error:
+        return error
+    if status in verdicts.keys():
+        return (f'У вас проверили работу "{homework_name}"!'
+                f'\n\n{verdicts[status]}')
     else:
-        return (logging.info('Работа пока не проверена'))
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        return (logging.error('Неизвестный статус домашки'))
 
 
 def get_homeworks(current_timestamp):
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
-    homework_statuses = requests.get(url, headers=headers, params=payload)
+    url_add = 'api/user_api/homework_statuses/'
+    homework_statuses = requests.get(Url + url_add,
+                                     headers=headers, params=payload)
     return homework_statuses.json()
 
 
 def send_message(message):
     logging.info('Отправлено сообщение')
-    return bot.send_message(chat_id=CHAT_ID, text=message)
+    return Bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
+    global HW_count
     logging.debug('Бот был запущен')
-    # случайная дата, с начала которой домашек не сдавалось
-    date = datetime.date(year=2020, month=8, day=23)
-    timestamp = time.mktime(date.timetuple())  # Начальное значение timestamp
+    # date = datetime.date(year=2020, month=8, day=23)
+    timestamp = 0  # time.mktime(date.timetuple())
     while True:
         try:
             homeworks = get_homeworks(int(timestamp))
-            if (homeworks['homeworks']):
+            # парсим только в случае если есть домашки, которые не прошли ревью
+            if (len(homeworks) > HW_count):
                 homework = homeworks['homeworks'][0]
                 if (homework['status'] != 'reviewing'):
                     text = parse_homework_status(homework)
                     send_message(text)
                     sys.exit()
-            else:
-                logging.info('Домашек.нет')
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
         except Exception as e:
             text = f'Бот упал с ошибкой: {e}'
